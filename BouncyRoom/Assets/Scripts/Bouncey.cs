e21dc24bbe31a6eff91f5320fafe4fff76985f8a
Kimiko Muwanguzi <kimikomu@gmail.com>
2018-11-28 21:16:54 -0500

    Changes

--- BouncyRoom/Assets/Scripts/Bouncey.cs
+++ BouncyRoom/Assets/Scripts/Bouncey.cs
@@ -15,18 +15,37 @@ public class Bouncey : MonoBehaviour {
     private Vector3 lastFrameVelocity;
     private Rigidbody ballRigidbody;
     private GameObject ball;
+    private Vector3 mouse;
 
-    private void OnEnable()
+    public GameObject tempParent;
+    public Transform grabber;
+
+
+    
+
+    void OnEnable()
     {
         ball = GameObject.FindGameObjectWithTag("ball");
         ballRigidbody = ball.GetComponent<Rigidbody>();
+
     }
 
-    private void OnMouseUp()
+    private void OnMouseDown()
     {
+        ballRigidbody.useGravity = false;
+        ballRigidbody.isKinematic = true;
+        ball.transform.position = grabber.transform.position;
+        ball.transform.rotation = grabber.transform.rotation;
+        ball.transform.parent = tempParent.transform;
+    }
 
+    void OnMouseUp()
+    {
+        ballRigidbody.useGravity = true;
         ballRigidbody.isKinematic = false;
         ballRigidbody.velocity = initialVelocity;
+        ball.transform.parent = null;
+        ball.transform.position = grabber.transform.position;
     }
 
     void Update()
